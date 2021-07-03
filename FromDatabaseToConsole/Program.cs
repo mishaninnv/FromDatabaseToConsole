d128a3cbc3854875ae3565280a8e16ee5a8a95a4
mishaninnv <mishaninnv93@gmail.com>
2021-07-03 07:15:55 +0500

    Refactoring.

--- FromDatabaseToConsole/Program.cs
+++ FromDatabaseToConsole/Program.cs
@@ -9,39 +9,62 @@ namespace FromDatabaseToConsole
     {
         static void Main(string[] args)
         {
+            if (args.Length < 1) return;
             var inputString = args[0];
-            var connString = @"Server=localhost\SQLEXPRESS01;Database=DataBaseTest;Trusted_Connection=True;";
-            var step = 5;
 
+            var connString = @"Server=localhost\SQLEXPRESS01;Database=DataBaseTest;Trusted_Connection=True;";
             var conn = new SqlConnection(connString);
-            conn.Open();
 
             var cmdString = string.Format(@"SELECT * FROM Words WHERE word LIKE '{0}%'", inputString);
 
+            var data = SelectDataFromDB(conn, cmdString);
+
+            if (data == null) return;
+            ConsoleOutput(data);
+        }
+
+        private static IEnumerable<KeyValuePair<string, int>> SelectDataFromDB(SqlConnection conn, string cmdString)
+        {
             using (var sqlCmd = new SqlCommand(cmdString, conn))
             {
+                conn.Open();
                 var reader = sqlCmd.ExecuteReader();
 
-                if (reader.HasRows)
-                {
-                    var result = new Dictionary<string, int>();
-
-                    while (reader.Read())
-                    {
-                        result.Add(reader.GetValue(0).ToString(), reader.GetInt32(1));
-                    }
-
-                    var result2 = result.OrderByDescending(s => s.Value).ThenBy(d => d.Key).Take(step);
-
-                    foreach (var word in result2)
-                    {
-                        Console.WriteLine($"Слово: {word.Key}, количество повторений: {word.Value}");
-                    }
-                }
-                else 
+                if (!reader.HasRows)
                 {
                     Console.WriteLine("Совпадений не найдено.");
+                    return null;
                 }
+
+                var data = GetReadData(reader);
+                var sortedData = SortData(data);
+
+                return sortedData;
+            }
+        }
+
+        private static Dictionary<string, int> GetReadData(SqlDataReader reader)
+        {
+            var result = new Dictionary<string, int>();
+
+            while (reader.Read())
+            {
+                result.Add(reader.GetValue(0).ToString(), reader.GetInt32(1));
+            }
+
+            return result;
+        }
+
+        private static IEnumerable<KeyValuePair<string, int>> SortData(Dictionary<string, int> data)
+        {
+            return data.OrderByDescending(s => s.Value).ThenBy(d => d.Key).Take(5);
+        }
+
+        private static void ConsoleOutput(IEnumerable<KeyValuePair<string,int>> keyValuePairs)
+        {
+            foreach (var element in keyValuePairs)
+            {
+                Console.WriteLine($"Слово: {element.Key}, количество повторений: {element.Value}");
             }
         }
     }
